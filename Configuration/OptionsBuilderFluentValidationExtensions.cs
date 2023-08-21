ef5921e631b77ed388f3a1b97283f15497d51327
Tera <24725862+teraa@users.noreply.github.com>
2023-08-21 17:53:32 +0200

    [skip ci] formatting

--- Configuration/OptionsBuilderFluentValidationExtensions.cs
+++ Configuration/OptionsBuilderFluentValidationExtensions.cs
@@ -16,7 +16,10 @@ public static class OptionsBuilderFluentValidationExtensions
     /// <typeparam name="TOptions">The options type to be configured.</typeparam>
     /// <param name="optionsBuilder">The options builder to add the services to.</param>
     /// <returns>The <see cref="OptionsBuilder{TOptions}"/> so that additional calls can be chained.</returns>
-    public static OptionsBuilder<TOptions> ValidateFluentValidation<[DynamicallyAccessedMembers(DynamicallyAccessedMemberTypes.PublicProperties | DynamicallyAccessedMemberTypes.NonPublicProperties)] TOptions>(this OptionsBuilder<TOptions> optionsBuilder) where TOptions : class
+    public static OptionsBuilder<TOptions> ValidateFluentValidation<
+        [DynamicallyAccessedMembers(DynamicallyAccessedMemberTypes.PublicProperties |
+                                    DynamicallyAccessedMemberTypes.NonPublicProperties)]
+        TOptions>(this OptionsBuilder<TOptions> optionsBuilder) where TOptions : class
     {
         optionsBuilder.Services.AddSingleton<IValidateOptions<TOptions>>(sp =>
         {
