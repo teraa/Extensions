221b2310d5e3341bc533fc6cc1f9f8f2b497c50e
Tera <24725862+teraa@users.noreply.github.com>
2024-02-21 11:57:00 +0100

    add identity extensions

---
+++ Identity/ModelBuilderExtensions.cs
@@ -0,0 +1,67 @@
+using JetBrains.Annotations;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace Teraa.Extensions.Identity;
+
+[PublicAPI]
+public static class ModelBuilderExtensions
+{
+    public static ModelBuilder UseSnakeCaseIdentityNames<TUser>(this ModelBuilder modelBuilder)
+        where TUser : IdentityUser<string>
+        => UseSnakeCaseIdentityNames
+        <
+            string,
+            TUser,
+            IdentityUserClaim<string>,
+            IdentityUserLogin<string>,
+            IdentityUserRole<string>,
+            IdentityUserToken<string>,
+            IdentityRole,
+            IdentityRoleClaim<string>
+        >(modelBuilder);
+
+    public static ModelBuilder UseSnakeCaseIdentityNames<
+        TKey,
+        TUser,
+        TUserClaim,
+        TUserLogin,
+        TUserRole,
+        TUserToken,
+        TRole,
+        TRoleClaim>(this ModelBuilder modelBuilder)
+        where TKey : IEquatable<TKey>
+        where TUser : IdentityUser<TKey>
+        where TUserClaim : IdentityUserClaim<TKey>
+        where TUserLogin : IdentityUserLogin<TKey>
+        where TUserRole : IdentityUserRole<TKey>
+        where TUserToken : IdentityUserToken<TKey>
+        where TRole : IdentityRole<TKey>
+        where TRoleClaim : IdentityRoleClaim<TKey>
+    {
+        // https://github.com/efcore/EFCore.NamingConventions/issues/2#issuecomment-612651161
+
+        modelBuilder.Entity<TRoleClaim>().ToTable("asp_net_role_claims");
+
+        modelBuilder.Entity<TRole>(b =>
+        {
+            b.ToTable("asp_net_roles");
+            b.HasIndex(e => e.NormalizedName).HasDatabaseName("ix_asp_net_roles_normalized_name");
+        });
+
+        modelBuilder.Entity<TUserClaim>().ToTable("asp_net_user_claims");
+        modelBuilder.Entity<TUserLogin>().ToTable("asp_net_user_logins");
+        modelBuilder.Entity<TUserRole>().ToTable("asp_net_user_roles");
+
+        modelBuilder.Entity<TUser>(b =>
+        {
+            b.ToTable("asp_net_users");
+            b.HasIndex(e => e.NormalizedUserName).HasDatabaseName("ix_asp_net_users_normalized_user_name");
+            b.HasIndex(e => e.NormalizedEmail).HasDatabaseName("ix_asp_net_users_normalized_email");
+        });
+
+        modelBuilder.Entity<TUserToken>().ToTable("asp_net_user_tokens");
+
+        return modelBuilder;
+    }
+}
