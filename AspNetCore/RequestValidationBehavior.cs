aa804fe7775c571829a8c1a74bb8abb9300ba43d
Tera <24725862+teraa@users.noreply.github.com>
2024-06-11 22:54:22 +0200

    add IH RequestValidationBehavior

---
+++ AspNetCore/RequestValidationBehavior.cs
@@ -0,0 +1,37 @@
+﻿using FluentValidation;
+using Immediate.Handlers.Shared;
+using JetBrains.Annotations;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Teraa.Extensions.AspNetCore;
+
+// Enable with
+// [assembly: Behaviors(typeof(RequestValidationBehavior<,>))]
+
+[PublicAPI]
+public sealed class RequestValidationBehavior<TRequest, TResponse>(IEnumerable<IValidator<TRequest>> validators)
+    : Behavior<TRequest, IActionResult>
+{
+    public override async ValueTask<IActionResult> HandleAsync(TRequest request, CancellationToken cancellationToken)
+    {
+        if (!validators.Any())
+            return await Next(request, cancellationToken);
+
+        var context = new ValidationContext<TRequest>(request);
+
+        var errors = validators
+            .Select(x => x.Validate(context))
+            .Where(x => !x.IsValid)
+            .SelectMany(x => x.Errors)
+            .GroupBy(x => x.PropertyName, x => x.ErrorMessage)
+            .ToDictionary(x => x.Key, x => x.ToArray());
+
+        if (errors.Count == 0)
+            return await Next(request, cancellationToken);
+
+        var problemDetails = new ValidationProblemDetails(errors);
+        var result = new BadRequestObjectResult(problemDetails);
+
+        return result;
+    }
+}
