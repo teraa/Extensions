2182a5e6bc902b96325cdc141caa0035cd1c0ad0
Tera <24725862+teraa@users.noreply.github.com>
2024-06-29 19:08:33 +0200

    add AddKeyedHttpMessageHandler extension method

--- AspNetCore/Extensions.cs
+++ AspNetCore/Extensions.cs
@@ -1,6 +1,7 @@
 using JetBrains.Annotations;
 using MediatR;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Http;
 
 namespace Teraa.Extensions.AspNetCore;
 
@@ -9,4 +10,15 @@ public static class Extensions
 {
     public static IServiceCollection AddRequestValidationBehaviour(this IServiceCollection services)
         => services.AddTransient(typeof(IPipelineBehavior<,>), typeof(RequestValidationBehaviour<,>));
+
+    public static IHttpClientBuilder AddKeyedHttpMessageHandler<THandler>(this IHttpClientBuilder builder, object key)
+        where THandler : DelegatingHandler
+    {
+        builder.Services.Configure<HttpClientFactoryOptions>(builder.Name, options =>
+        {
+            options.HttpMessageHandlerBuilderActions.Add(b => b.AdditionalHandlers.Add(b.Services.GetRequiredKeyedService<THandler>(key)));
+        });
+
+        return builder;
+    }
 }
